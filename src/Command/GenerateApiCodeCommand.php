e4e122fb8ff1906a676838900102805ffe67b66e
Patrik Foldes <studio22@mail.ru>
2020-02-05 16:41:15 +0300

    Implemented caching of parsed specifications

--- src/Command/GenerateApiCodeCommand.php
+++ src/Command/GenerateApiCodeCommand.php
@@ -5,9 +5,11 @@ declare(strict_types=1);
 namespace OnMoon\OpenApiServerBundle\Command;
 
 use OnMoon\OpenApiServerBundle\CodeGenerator\ApiServerCodeGenerator;
+use OnMoon\OpenApiServerBundle\Specification\SpecificationLoader;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Contracts\Cache\TagAwareCacheInterface;
 use function Safe\sprintf;
 
 class GenerateApiCodeCommand extends Command
@@ -15,14 +17,17 @@ class GenerateApiCodeCommand extends Command
     public const COMMAND = 'open-api:generate';
 
     private ApiServerCodeGenerator $apiServerCodeGenerator;
+    private TagAwareCacheInterface $cache;
     private string $rootPath;
 
     public function __construct(
         ApiServerCodeGenerator $apiServerCodeGenerator,
+        TagAwareCacheInterface $cache,
         string $rootPath,
         ?string $name = null
     ) {
         $this->apiServerCodeGenerator = $apiServerCodeGenerator;
+        $this->cache                  = $cache;
         $this->rootPath               = $rootPath;
 
         parent::__construct($name);
@@ -41,6 +46,7 @@ class GenerateApiCodeCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output) : int
     {
+        $this->cache->invalidateTags([SpecificationLoader::CACHE_TAG]);
         $this->apiServerCodeGenerator->generate();
 
         $output->writeln(sprintf('API server code generated in: %s', $this->rootPath));
