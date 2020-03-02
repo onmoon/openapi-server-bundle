e7009c550fcd413383d7335e94c7a51d956a9f01
DimanKuskov <dimankuskov@gmail.com>
2020-03-02 12:49:32 +0300

    Added DTO Mapper (#45)
    * Added mapper
    
    * Fixed errors
    
    * Added propertyMapper

---
+++ src/Exception/CannotMapToDto.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OnMoon\OpenApiServerBundle\Exception;
+
+use function Safe\sprintf;
+
+class CannotMapToDto extends OpenApiError
+{
+    public static function becausePhpDocIsCorrupt(string $name, string $class) : self
+    {
+        return new self(
+            sprintf(
+                'PhpDoc is not readable for "%s" in "%s"',
+                $name,
+                $class
+            )
+        );
+    }
+
+    public static function becauseUnknownType(string $type, string $name, string $class) : self
+    {
+        return new self(
+            sprintf(
+                'Type "%s" is unknown for "%s" in "%s"',
+                $type,
+                $name,
+                $class
+            )
+        );
+    }
+
+    public static function becauseClassDoesNotExist(string $childClass, string $name, string $class) : self
+    {
+        return new self(
+            sprintf(
+                'Class "%s" does not exist for "%s" in "%s"',
+                $childClass,
+                $name,
+                $class
+            )
+        );
+    }
+
+    public static function becauseRootClassDoesNotExist(string $class) : self
+    {
+        return new self(
+            sprintf(
+                'Class "%s" does not exist',
+                $class
+            )
+        );
+    }
+}
