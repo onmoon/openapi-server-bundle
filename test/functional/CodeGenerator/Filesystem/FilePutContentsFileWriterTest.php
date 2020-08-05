8e7f216f302501c56aa2961da05eada3b5365edb
Dmitry Podtyagin <dmitry.podtyagin@gmail.com>
2020-08-05 18:33:40 +0300

    #95 Functional tests for OnMoon\OpenApiServerBundle\CodeGenerator\Filesystem (#124)
    * Tests for FileWriter
    
    * csfix
    
    * Improve assertions
    
    * Change namespace
    
    * Change incorrect @covers
    
    Co-authored-by: Dmitry Podtyagin <podtyagin@csgo.com>
    Co-authored-by: Dmitry <d.podtyagin@yclients.tech>

---
+++ test/functional/CodeGenerator/Filesystem/FilePutContentsFileWriterTest.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OnMoon\OpenApiServerBundle\Test\Functional\CodeGenerator\Filesystem;
+
+use OnMoon\OpenApiServerBundle\CodeGenerator\Filesystem\FilePutContentsFileWriter;
+use PHPUnit\Framework\Assert;
+use PHPUnit\Framework\TestCase;
+
+use function Safe\file_get_contents;
+use function Safe\rmdir;
+use function Safe\unlink;
+
+use const DIRECTORY_SEPARATOR;
+
+/**
+ * @covers \OnMoon\OpenApiServerBundle\CodeGenerator\Filesystem\FilePutContentsFileWriter
+ */
+final class FilePutContentsFileWriterTest extends TestCase
+{
+    public function testWriteCreatesFile(): void
+    {
+        $path        = 'someRandomDir';
+        $filename    = 'testFilename.txt';
+        $fullPath    = $path . DIRECTORY_SEPARATOR . $filename;
+        $fileContent = 'Some Random Content';
+
+        $fileWriter = new FilePutContentsFileWriter(0755);
+
+        Assert::assertFileDoesNotExist($fullPath);
+        Assert::assertDirectoryDoesNotExist($path);
+
+        $fileWriter->write($path, $filename, $fileContent);
+
+        Assert::assertDirectoryExists($path);
+        Assert::assertFileExists($fullPath);
+        Assert::assertEquals($fileContent, file_get_contents($fullPath));
+
+        unlink($fullPath);
+        rmdir($path);
+    }
+}
