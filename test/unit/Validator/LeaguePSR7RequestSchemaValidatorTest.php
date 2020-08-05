374de9c8b49ff54d9845238421c0106c1ea664f3
Egor Zotov <me@egor.work>
2020-08-05 18:32:23 +0300

    #103 Tests for OnMoon\OpenApiServerBundle\Validator\LeaguePSR7Request… (#122)
    * #103 Tests for OnMoon\OpenApiServerBundle\Validator\LeaguePSR7RequestSchemaValidator
    
    * add covers annotation
    
    * #103 unit tests for league LeaguePSR7RequestSchemaValidator

---
+++ test/unit/Validator/LeaguePSR7RequestSchemaValidatorTest.php
@@ -0,0 +1,83 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OnMoon\OpenApiServerBundle\Test\Unit\Validator;
+
+use cebe\openapi\spec\OpenApi;
+use League\OpenAPIValidation\PSR7\OperationAddress;
+use League\OpenAPIValidation\PSR7\RoutedServerRequestValidator;
+use League\OpenAPIValidation\PSR7\ValidatorBuilder;
+use OnMoon\OpenApiServerBundle\Specification\Definitions\Operation;
+use OnMoon\OpenApiServerBundle\Specification\Definitions\Specification;
+use OnMoon\OpenApiServerBundle\Validator\LeaguePSR7RequestSchemaValidator;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ServerRequestInterface;
+use Symfony\Bridge\PsrHttpMessage\Factory\PsrHttpFactory;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * @covers \OnMoon\OpenApiServerBundle\Validator\LeaguePSR7RequestSchemaValidator
+ */
+class LeaguePSR7RequestSchemaValidatorTest extends TestCase
+{
+    public function testValidate(): void
+    {
+        $operationId = 'test';
+        $openApi     = new OpenApi([]);
+        $request     = new Request();
+
+        $operation = $this->createMock(Operation::class);
+        $operation
+            ->expects(self::once())
+            ->method('getUrl')
+            ->willReturn('test_url');
+        $operation
+            ->expects(self::once())
+            ->method('getMethod')
+            ->willReturn('test_method');
+
+        $specification = $this->createMock(Specification::class);
+        $specification
+            ->expects(self::once())
+            ->method('getOperation')
+            ->with($operationId)
+            ->willReturn($operation);
+
+        $specification
+            ->expects(self::once())
+            ->method('getOpenApi')
+            ->willReturn($openApi);
+
+        $validatorBuilderMock = $this->createMock(ValidatorBuilder::class);
+        $validatorBuilderMock
+            ->expects(self::once())
+            ->method('fromSchema')
+            ->with($openApi)
+            ->willReturn($validatorBuilderMock);
+
+        $routedServerRequestValidator = $this->createMock(RoutedServerRequestValidator::class);
+
+        $validatorBuilderMock
+            ->expects(self::once())
+            ->method('getRoutedRequestValidator')
+            ->willReturn($routedServerRequestValidator);
+
+        $serverRequestInterfaceStub = $this->createStub(ServerRequestInterface::class);
+
+        $psrHttpFactory = $this->createMock(PsrHttpFactory::class);
+        $psrHttpFactory
+            ->expects(self::once())
+            ->method('createRequest')
+            ->with($request)
+            ->willReturn($serverRequestInterfaceStub);
+
+        $routedServerRequestValidator
+            ->expects(self::once())
+            ->method('validate')
+            ->with(new OperationAddress('test_url', 'test_method'));
+
+        $leaguePSR7RequestSchemaValidator = new LeaguePSR7RequestSchemaValidator($validatorBuilderMock, $psrHttpFactory);
+        $leaguePSR7RequestSchemaValidator->validate($request, $specification, $operationId);
+    }
+}
