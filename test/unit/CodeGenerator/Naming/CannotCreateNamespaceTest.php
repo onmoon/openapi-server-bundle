7b70b9e64ba3dc133371e38c021c027d8ef311f9
Dmitry Podtyagin <dmitry.podtyagin@gmail.com>
2020-08-12 19:10:00 +0300

    #79 Tests for OnMoon\OpenApiServerBundle\CodeGenerator\Naming (#134)
    * Tests for CodeGenerator\Naming
    
    * csfix

---
+++ test/unit/CodeGenerator/Naming/CannotCreateNamespaceTest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OnMoon\OpenApiServerBundle\Test\Unit\CodeGenerator\Naming;
+
+use OnMoon\OpenApiServerBundle\CodeGenerator\Naming\CannotCreateNamespace;
+use PHPUnit\Framework\TestCase;
+
+use function Safe\sprintf;
+
+/**
+ * @covers  \OnMoon\OpenApiServerBundle\CodeGenerator\Naming\CannotCreateNamespace
+ */
+class CannotCreateNamespaceTest extends TestCase
+{
+    public function testBecauseTestContainsNoValidSymbolsShowCorrectError(): void
+    {
+        $text = 'Some random text';
+
+        $exceptionMessage = sprintf(
+            'Cannot create namespace from text: %s. Text contains no characters that can be used.',
+            $text
+        );
+
+        $this->expectException(CannotCreateNamespace::class);
+        $this->expectExceptionMessage($exceptionMessage);
+
+        throw CannotCreateNamespace::becauseTextContainsNoValidSymbols($exceptionMessage);
+    }
+}
---
+++ test/unit/CodeGenerator/Naming/CannotCreatePropertyNameTest.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OnMoon\OpenApiServerBundle\Test\Unit\CodeGenerator\Naming;
+
+use OnMoon\OpenApiServerBundle\CodeGenerator\Naming\CannotCreatePropertyName;
+use PHPUnit\Framework\TestCase;
+
+use function Safe\sprintf;
+
+/**
+ * @covers \OnMoon\OpenApiServerBundle\CodeGenerator\Naming\CannotCreatePropertyName
+ */
+final class CannotCreatePropertyNameTest extends TestCase
+{
+    public function testBecauseTextContainsNoValidSymbolsShowsCorrectError(): void
+    {
+        $text = 'Some random text';
+
+        $exceptionMessage = sprintf(
+            'Cannot create property name from text: %s. Text contains no characters that can be used.',
+            $text
+        );
+
+        $this->expectException(CannotCreatePropertyName::class);
+        $this->expectExceptionMessage($exceptionMessage);
+
+        throw CannotCreatePropertyName::becauseTextContainsNoValidSymbols($exceptionMessage);
+    }
+
+    public function testBecauseIsNotValidPhpPropertyNameShowsCorrectError(): void
+    {
+        $text = 'Some random text';
+
+        $exceptionMessage = sprintf(
+            'Cannot create property name from: %s. String is not a valid PHP property name.',
+            $text
+        );
+
+        $this->expectException(CannotCreatePropertyName::class);
+        $this->expectExceptionMessage($exceptionMessage);
+
+        throw CannotCreatePropertyName::becauseIsNotValidPhpPropertyName($exceptionMessage);
+    }
+}
