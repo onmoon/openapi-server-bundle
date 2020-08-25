fd862590e786fb3097330935917a5dc90004c246
egor zotov <me@egor.work>
2020-08-25 22:02:02 +0300

    Tests for OnMoon\OpenApiServerBundle\CodeGenerator\PhpParserGenerators\InterfaceCodeGenerator

---
+++ test/unit/CodeGenerator/PhpParserGenerators/InterfaceCodeGeneratorTest.php
@@ -0,0 +1,138 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OnMoon\OpenApiServerBundle\Test\Unit\CodeGenerator\PhpParserGenerators;
+
+use OnMoon\OpenApiServerBundle\CodeGenerator\Definitions\ClassDefinition;
+use OnMoon\OpenApiServerBundle\CodeGenerator\Definitions\GeneratedFileDefinition;
+use OnMoon\OpenApiServerBundle\CodeGenerator\Definitions\GeneratedInterfaceDefinition;
+use OnMoon\OpenApiServerBundle\CodeGenerator\Definitions\RequestHandlerInterfaceDefinition;
+use OnMoon\OpenApiServerBundle\CodeGenerator\PhpParserGenerators\InterfaceCodeGenerator;
+use OnMoon\OpenApiServerBundle\Interfaces\ResponseDto;
+use OnMoon\OpenApiServerBundle\Types\ScalarTypesResolver;
+use PhpParser\BuilderFactory;
+use PHPUnit\Framework\Assert;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \OnMoon\OpenApiServerBundle\CodeGenerator\PhpParserGenerators\InterfaceCodeGenerator
+ */
+class InterfaceCodeGeneratorTest extends TestCase
+{
+    private InterfaceCodeGenerator $interfaceCodeGenerator;
+
+    protected function setUp(): void
+    {
+        $builderFactory               = new BuilderFactory();
+        $scalarTypeResolver           = new ScalarTypesResolver();
+        $languageLevel                = '7.4';
+        $fullDocs                     = true;
+        $this->interfaceCodeGenerator = new InterfaceCodeGenerator(
+            $builderFactory,
+            $scalarTypeResolver,
+            $languageLevel,
+            $fullDocs,
+        );
+    }
+
+    protected function tearDown(): void
+    {
+        unset($this->interfaceCodeGenerator);
+        parent::tearDown();
+    }
+
+    public function testGenerateWithNotExtendedGeneratedInterfaceDefinition(): void
+    {
+        $generatedInterfaceDefinition = new GeneratedInterfaceDefinition();
+        $generatedInterfaceDefinition->setNamespace('Test\Test2');
+        $generatedInterfaceDefinition->setClassName('TestClass');
+
+        $generatedCode                   =
+            <<<EOD
+<?php
+
+declare (strict_types=1);
+namespace Test\Test2;
+
+/**
+ * This interface was automatically generated
+ * You should not change it manually as it will be overwritten
+ */
+interface TestClass
+{
+}
+EOD;
+        $expectedGeneratedFileDefinition = new GeneratedFileDefinition(
+            $generatedInterfaceDefinition,
+            $generatedCode
+        );
+        $generatedFileDefinition         = $this->interfaceCodeGenerator->generate($generatedInterfaceDefinition);
+
+        Assert::assertEquals($expectedGeneratedFileDefinition, $generatedFileDefinition);
+    }
+
+    public function testGenerateWithExtendedGeneratedInterfaceDefinition(): void
+    {
+        $generatedInterfaceDefinition = new GeneratedInterfaceDefinition();
+        $generatedInterfaceDefinition->setExtends(ClassDefinition::fromFQCN(ResponseDto::class));
+        $generatedInterfaceDefinition->setNamespace('Test\Test2');
+        $generatedInterfaceDefinition->setClassName('TestClass');
+
+        $generatedCode                   =
+            <<<EOD
+<?php
+
+declare (strict_types=1);
+namespace Test\Test2;
+
+use OnMoon\OpenApiServerBundle\Interfaces\ResponseDto;
+/**
+ * This interface was automatically generated
+ * You should not change it manually as it will be overwritten
+ */
+interface TestClass extends ResponseDto
+{
+}
+EOD;
+        $expectedGeneratedFileDefinition = new GeneratedFileDefinition(
+            $generatedInterfaceDefinition,
+            $generatedCode
+        );
+        $generatedFileDefinition         = $this->interfaceCodeGenerator->generate($generatedInterfaceDefinition);
+
+        Assert::assertEquals($expectedGeneratedFileDefinition, $generatedFileDefinition);
+    }
+
+    public function testGenerateWithRequestHandlerInterfaceDefinition(): void
+    {
+        $generatedInterfaceDefinition = new RequestHandlerInterfaceDefinition();
+        $generatedInterfaceDefinition->setNamespace('Test\Test2');
+        $generatedInterfaceDefinition->setClassName('TestClass');
+        $generatedInterfaceDefinition->setMethodName('test');
+
+        $generatedCode                   =
+            <<<EOD
+<?php
+
+declare (strict_types=1);
+namespace Test\Test2;
+
+/**
+ * This interface was automatically generated
+ * You should not change it manually as it will be overwritten
+ */
+interface TestClass
+{
+    public function test() : void;
+}
+EOD;
+        $expectedGeneratedFileDefinition = new GeneratedFileDefinition(
+            $generatedInterfaceDefinition,
+            $generatedCode
+        );
+        $generatedFileDefinition         = $this->interfaceCodeGenerator->generate($generatedInterfaceDefinition);
+
+        Assert::assertEquals($expectedGeneratedFileDefinition, $generatedFileDefinition);
+    }
+}
