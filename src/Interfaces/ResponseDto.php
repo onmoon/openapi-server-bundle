361526b9fba6a2bc5cd82f47958ce776e913c920
Patrik Foldes <studio22@mail.ru>
2022-04-11 17:22:15 +0300

    fix

--- src/Interfaces/ResponseDto.php
+++ src/Interfaces/ResponseDto.php
@@ -7,8 +7,5 @@ namespace OnMoon\OpenApiServerBundle\Interfaces;
 // phpcs:disable PSR2.Methods.MethodDeclaration.Underscore
 interface ResponseDto extends Dto
 {
-    /**
-     * @internal
-     */
     public static function _getResponseCode(): string;
 }
