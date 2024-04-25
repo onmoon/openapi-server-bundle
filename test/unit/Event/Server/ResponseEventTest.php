41d32ea15cd8c2846f9cfcedc9d6a5cabb9ecc61
samoylenko <stas@csgo.com>
2024-04-25 19:08:14 +0300

    addded additional data to response event

--- test/unit/Event/Server/ResponseEventTest.php
+++ test/unit/Event/Server/ResponseEventTest.php
@@ -6,9 +6,11 @@ namespace OnMoon\OpenApiServerBundle\Test\Unit\Event\Server;
 
 use cebe\openapi\spec\OpenApi;
 use OnMoon\OpenApiServerBundle\Event\Server\ResponseEvent;
+use OnMoon\OpenApiServerBundle\Interfaces\RequestHandler;
 use OnMoon\OpenApiServerBundle\Specification\Definitions\Specification;
 use PHPUnit\Framework\Assert;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
@@ -18,14 +20,19 @@ final class ResponseEventTest extends TestCase
 {
     public function testResponseEventGettersReturnCorrectValues(): void
     {
-        $response      = new Response();
-        $operationId   = '12345';
-        $specification = new Specification([], [], new OpenApi([]));
+        $response       = new Response();
+        $operationId    = '12345';
+        $specification  = new Specification([], [], new OpenApi([]));
+        $requestHandler = new class () implements RequestHandler{
+        };
+        $request        = new Request();
 
-        $responseEvent = new ResponseEvent($response, $operationId, $specification);
+        $responseEvent = new ResponseEvent($response, $operationId, $specification, $requestHandler, $request);
 
         Assert::assertEquals($response, $responseEvent->getResponse());
         Assert::assertEquals($operationId, $responseEvent->getOperationId());
         Assert::assertEquals($specification, $responseEvent->getSpecification());
+        Assert::assertEquals($requestHandler, $responseEvent->getRequestHandler());
+        Assert::assertEquals($request, $responseEvent->getRequest());
     }
 }
